f55bb6d55cc8ee3d016ade799c82824fb44b7d1a
Saefstroem <alexandersafstrom@proton.me>
2025-11-30 22:12:10 +0800

    Add custom structs

--- examples/sha/src/main.rs
+++ examples/sha/src/main.rs
@@ -1,20 +1,59 @@
 // Copyright 2025 RISC Zero, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
 
+use borsh::{BorshDeserialize, BorshSerialize};
 use clap::Parser;
-use risc0_zkvm::{default_prover, sha::Digest, ExecutorEnv, Receipt};
+use risc0_zkvm::ProverOpts;
+use risc0_zkvm::recursion::MerkleProof;
+use risc0_zkvm::sha::Digestible;
+use risc0_zkvm::{ExecutorEnv, Receipt, default_prover, sha::Digest};
 use sha_methods::{HASH_ELF, HASH_ID, HASH_RUST_CRYPTO_ELF};
+use std::fs::File;
+use std::io::Write;
+
+/// A receipt composed of a Groth16 over the BN_254 curve
+#[derive(Clone, Debug, BorshSerialize, BorshDeserialize)]
+#[cfg_attr(test, derive(PartialEq))]
+pub struct SuccinctReceipt {
+    /// The cryptographic seal of this receipt. This seal is a STARK proving an execution of the
+    /// recursion circuit.
+    seal: Vec<u32>,
+
+    /// The control ID of this receipt, identifying the recursion program that was run (e.g. lift,
+    /// join, or resolve).
+    control_id: Digest,
+
+    /// Claim containing information about the computation that this receipt proves.
+    ///
+    /// The standard claim type is [ReceiptClaim][crate::ReceiptClaim], which represents a RISC-V
+    /// zkVM execution.
+    claim: Digest,
+
+    /// Name of the hash function used to create this receipt.
+    hashfn: String,
+
+    /// A digest of the verifier parameters that can be used to verify this receipt.
+    ///
+    /// Acts as a fingerprint to identify differing proof system or circuit versions between a
+    /// prover and a verifier. It is not intended to contain the full verifier parameters, which must
+    /// be provided by a trusted source (e.g. packaged with the verifier code).
+    verifier_parameters: Digest,
+
+    /// Merkle inclusion proof for control_id against the control root for this receipt.
+    control_inclusion_proof: MerkleProof,
+}
 
 /// Hash the given bytes, returning the digest and a [Receipt] that can
 /// be used to verify that the hash was computed correctly (i.e. that
@@ -43,7 +82,14 @@ fn provably_hash(input: &str, use_rust_crypto: bool) -> (Digest, Receipt) {
     let prover = default_prover();
 
     // Produce a receipt by proving the specified ELF binary.
-    let receipt = prover.prove(env, elf).unwrap().receipt;
+    let receipt = prover
+        .prove_with_opts(env, elf, &ProverOpts::succinct())
+        .unwrap()
+        .receipt;
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
+    receipt.verify(HASH_ID).unwrap();
 
     let digest = receipt.journal.decode().unwrap();
     (digest, receipt)
@@ -60,9 +106,40 @@ fn main() {
     let args = Cli::parse();
 
     // Prove hash the message.
-    let (digest, receipt) = provably_hash(&args.message, false);
+    let (_digest, receipt) = provably_hash(&args.message, false);
 
+    let inner_receipt = receipt.inner.succinct().unwrap();
+
+    // Convert the inner receipt to our custom SuccinctReceipt type
+    let custom_receipt = SuccinctReceipt {
+        control_id: inner_receipt.control_id,
+        seal: inner_receipt.seal.clone(),
+        hashfn: inner_receipt.hashfn.clone(),
+        control_inclusion_proof: inner_receipt.control_inclusion_proof.clone(),
+        claim: inner_receipt.claim.digest(),
+        verifier_parameters: inner_receipt.verifier_parameters,
+    };
+
+    println!("Seal size in bytes: {}", custom_receipt.seal.len() * 4);
+
+    // Hex encode the serialized receipt
+    let hex_encoded = hex::encode(
+        borsh::to_vec(&custom_receipt).expect("Failed to serialize receipt with Borsh"),
+    );
+
+    // Write to proof.hex file
+    let mut file = File::create("succinct.hex").expect("Failed to create proof.hex file");
+    file.write_all(hex_encoded.as_bytes())
+        .expect("Failed to write to proof.hex file");
+
+    println!("Hex-encoded proof written to proof.hex");
+
+    // Here is where one would send 'hex_encoded' over the network...
+    println!("hashid:{:?}", HASH_ID);
     // Verify the receipt, ensuring the prover knows a valid SHA-256 preimage.
+
+    let digest = receipt.journal.digest();
+    println!("journal digest:{:?}", digest);
     receipt
         .verify(HASH_ID)
         .expect("receipt verification failed");
@@ -73,6 +150,18 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use sha_methods::{HASH_ID, HASH_RUST_CRYPTO_ID};
+
+    // [1423791584, 880512669, 1738307172, 2533723364, 3880046003, 402541997, 1959133478, 277067013]
+    use risc0_zkvm::Digest;
+    #[test]
+    fn hash_abcd() {
+        let digest = Digest::new([
+            2490666878, 931064206, 2859705260, 2816403364, 2239188247, 1314886651, 35655229,
+            3459784930,
+        ]);
+
+        println!("sha succinct image id hex:{:?}", digest);
+    }
 
     #[test]
     fn hash_abc() {
@@ -86,6 +175,7 @@ mod tests {
     }
 
     #[test]
+    #[gpu_guard::gpu_guard(skip_if_dev_mode = true)]
     fn hash_abc_rust_crypto() {
         let (digest, receipt) = super::provably_hash("abc", true);
         receipt.verify(HASH_RUST_CRYPTO_ID).unwrap();
